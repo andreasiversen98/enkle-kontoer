9cb0906a9bb1878a55b3f02a9f32ec71342cfe95
andreasiversen98 <andreasiversen98@protonmail.com>
2026-03-31 20:16:20 +0200

    Added an input field to the view for entering a custom amount

--- controller.js
+++ controller.js
@@ -2,3 +2,9 @@ function changeBalance(amount) {
   bankAccount.changeBalance(amount);
   updateView();
 }
+
+function addCustomAmount(inputValue) {
+  const amount = Number(inputValue);
+  bankAccount.changeBalance(amount);
+  updateView();
+}
--- view.js
+++ view.js
@@ -8,5 +8,9 @@ function updateView() {
   <button onclick="changeBalance(5000)">Legg til 5000 kr</button>
   <button onclick="changeBalance(-5000)">Ta ut 5000 kr</button>
   <button onclick="changeBalance(-30000)">Ta ut 30000 kr</button>
+  <input 
+    placeholder="Egendefinert beløp"
+    type="number" 
+    onchange="addCustomAmount(this.value)" />
   `;
 }
